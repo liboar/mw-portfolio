cf9b1dcc694754d08e39f2005eaba53410553c32
bjoern <bjoern>
2020-09-10 13:50:13 +0300

    changes in  data-netlify shop form

--- src/components/shopItemForm.js
+++ src/components/shopItemForm.js
@@ -17,63 +17,73 @@ const ShopItemForm = props => {
   const { price, description, type, fluid, key, size, toggle } = props
 
   return (
-    <form hidden name="shop"
-      method="POST"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field">
-              <input type="hidden" name="form-name" value="shop" />
-              <input type="phone" name="phone" id="phone" placeholder="Ihre Mobilfunknummer"/>
-      </form>
-    <Form
-      name="shop"
-      method="POST"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-    >
-      <input type="hidden" name="form-name" value="shop" />
-      <ModalBody style={{ textAlign: "right" }}>
-        <div className="shopFormSummary">
-          <FormGroup>
-            <Input type="select" name="select" id="exampleSelect">
-              <option>
-                {description} {size} {type}
-                shopping checkout form
-              </option>
-            </Input>
-            <div>{price} &#8381;</div>
-          </FormGroup>
-          <FormGroup
-            id="right"
-            style={{ alignSelf: "right", paddingTop: "1rem" }}
-          >
-            <div>
-              <Input
-                type="phone"
-                name="phone"
-                id="phone"
-                placeholder="Ihre Mobilfunknummer"
-              />
-            </div>
-          </FormGroup>
-        </div>
-      </ModalBody>
-      <ModalBody>
-        {" "}
-        <Img
-          fluid={fluid}
-          alt="no img"
-          style={{
-            margin: "1rem",
-          }}
+    <>
+      <form
+        hidden
+        name="shop"
+        method="POST"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="form-name" value="shop" />
+        <input
+          type="phone"
+          name="phone"
+          id="phone"
+          placeholder="Ihre Mobilfunknummer"
         />
-      </ModalBody>{" "}
-      <ModalFooter>
-        <Button color="primary">Bestellung senden</Button>
-        <Button color="secondary" onClick={toggle}>
-          Abbrechen
-        </Button>
-      </ModalFooter>
-    </Form>
+      </form>
+      <Form
+        name="shop"
+        method="POST"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="form-name" value="shop" />
+        <ModalBody style={{ textAlign: "right" }}>
+          <div className="shopFormSummary">
+            <FormGroup>
+              <Input type="select" name="select" id="exampleSelect">
+                <option>
+                  {description} {size} {type}
+                  shopping checkout form
+                </option>
+              </Input>
+              <div>{price} &#8381;</div>
+            </FormGroup>
+            <FormGroup
+              id="right"
+              style={{ alignSelf: "right", paddingTop: "1rem" }}
+            >
+              <div>
+                <Input
+                  type="phone"
+                  name="phone"
+                  id="phone"
+                  placeholder="Ihre Mobilfunknummer"
+                />
+              </div>
+            </FormGroup>
+          </div>
+        </ModalBody>
+        <ModalBody>
+          {" "}
+          <Img
+            fluid={fluid}
+            alt="no img"
+            style={{
+              margin: "1rem",
+            }}
+          />
+        </ModalBody>{" "}
+        <ModalFooter>
+          <Button color="primary">Bestellung senden</Button>
+          <Button color="secondary" onClick={toggle}>
+            Abbrechen
+          </Button>
+        </ModalFooter>
+      </Form>
+    </>
   )
 }
 
