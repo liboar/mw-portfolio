b469d54a33fad5c3a21643c3c5a8102778e6b457
bjoern <bjoern>
2020-09-10 13:21:06 +0300

    changes in  data-netlify shop form

--- src/components/shopItemForm.js
+++ src/components/shopItemForm.js
@@ -23,32 +23,34 @@ const ShopItemForm = props => {
       data-netlify="true"
       data-netlify-honeypot="bot-field"
     >
-      <input type="hidden" name="shop" value="shop" />
-
-      {/* <ModalBody style={{ textAlign: "right" }}> */}
-      {/* <div className="shopFormSummary"> */}
-      <FormGroup>
-        <Input type="select" name="select" id="exampleSelect">
-          <option>
-            {description} {size} {type}
-            shopping checkout form
-          </option>
-        </Input>
-        <div>{price} &#8381;</div>
-      </FormGroup>
-      <FormGroup id="right" style={{ alignSelf: "right", paddingTop: "1rem" }}>
-        <div>
-          <Input
-            type="phone"
-            name="phone"
-            id="phone"
-            placeholder="Ihre Mobilfunknummer"
-          />
+      <input type="hidden" name="form-name" value="shop" />
+      <ModalBody style={{ textAlign: "right" }}>
+        <div className="shopFormSummary">
+          <FormGroup>
+            <Input type="select" name="select" id="exampleSelect">
+              <option>
+                {description} {size} {type}
+                shopping checkout form
+              </option>
+            </Input>
+            <div>{price} &#8381;</div>
+          </FormGroup>
+          <FormGroup
+            id="right"
+            style={{ alignSelf: "right", paddingTop: "1rem" }}
+          >
+            <div>
+              <Input
+                type="phone"
+                name="phone"
+                id="phone"
+                placeholder="Ihre Mobilfunknummer"
+              />
+            </div>
+          </FormGroup>
         </div>
-      </FormGroup>
-      {/* </div>
-      </ModalBody> */}
-      {/* <ModalBody>
+      </ModalBody>
+      <ModalBody>
         {" "}
         <Img
           fluid={fluid}
@@ -57,14 +59,14 @@ const ShopItemForm = props => {
             margin: "1rem",
           }}
         />
-      </ModalBody> */}
-
-      {/* <ModalFooter> */}
-      <Button color="primary">Bestellung senden</Button>
-      <Button color="secondary" onClick={toggle}>
-        Abbrechen
-      </Button>
-      {/* </ModalFooter> */}
+      </ModalBody>
+      /*{" "}
+      <ModalFooter>
+        <Button color="primary">Bestellung senden</Button>
+        <Button color="secondary" onClick={toggle}>
+          Abbrechen
+        </Button>
+      </ModalFooter>
     </Form>
   )
 }
