96d1aadebc6436fe1e9275997eb350ebf4af1a9e
bjoern <bjoern>
2020-09-17 18:49:42 +0300

    google analytics in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,7 +98,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-178298285-1",
+        trackingId: "UA-178298167-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
