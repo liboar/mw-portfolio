6c0cfe4bf088c9f7b70a5cba3ecc1e0d5629e714
bjoern <bjoern>
2020-09-17 18:51:10 +0300

    google analytics in gatsby-config, set head to true

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,9 +98,9 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-178298167-1",
+        trackingId: "UA-178298167-1 ",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
